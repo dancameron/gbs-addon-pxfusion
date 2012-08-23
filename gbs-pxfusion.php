6c4fbbf4722a507e43eea0625e7b890fde9130fe
Dan Cameron <dan@sproutventure.com>
2012-08-23 15:33:27 -0700

    Starter kit
    The start of every processor is a good base to kick off.

---
+++ gbs-pxfusion.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Plugin Name: Group Buying Payment Processor - PxFusion
+Version: Beta
+Plugin URI: http://sproutventure.com/wordpress/group-buying
+Description: PxFusion Add-on.
+Author: Sprout Venture
+Author URI: http://sproutventure.com/wordpress
+Plugin Author: Dan Cameron
+Contributors: Dan Cameron
+Text Domain: group-buying
+Domain Path: /lang
+*/
+
+add_action('gb_register_processors', 'gb_load_pxfusion');
+
+function gb_load_pxfusion() {
+	require_once('groupBuyingPxFusion.class.php');
+}
